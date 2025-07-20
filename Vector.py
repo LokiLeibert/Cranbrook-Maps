a5e7779ba3fc8831c47deb3e20f918175fb1c90a
LokiLeibert <loki.leibert@gmail.com>
2025-07-20 17:30:04 +1000

    Update Vector.py

--- Vector.py
+++ Vector.py
@@ -1,79 +1,110 @@
+# Route Finder Mobile
 # Vector.py
-# Version 29 January 2025 20:20
+# 29 January 2025 20:20
 
-# Simple vector math utilities
-
-import math
+# Simple vector class for vertex locations and vector maths
 
 
-def v_fn(fn, vectors):
-    return [fn(*vals) for vals in zip(*vectors)]
+import numpy as np
 
 
-def v_add(*vectors):
-    return v_fn(lambda *v: sum(v), vectors)
+class Vector:
+    def __init__(self, x, y):
+        self.array = np.array([x, y])
 
+    @staticmethod
+    def _check_vector_type(other):
+        if not isinstance(other, Vector):
+            raise TypeError('Operation is defined only for type Vector')
 
-def v_sub(v1, v2):
-    return v_fn(lambda i, j: i-j, [v1, v2])
+    def __add__(self, other):
+        self._check_vector_type(other)
+        return Vector(*(self.array + other.array))
 
+    def __sub__(self, other):
+        self._check_vector_type(other)
+        return Vector(*(self.array - other.array))
 
-def v_mul(*vectors):
-    return v_fn(lambda *v: math.prod(v), vectors)
+    def __mul__(self, other):
+        if isinstance(other, Vector):
+            return Vector(*(self.array * other.array))
+        elif isinstance(other, int) or isinstance(other, float):
+            return Vector(*(self.array * other))
+        else:
+            self._check_vector_type(other)
 
+    def __truediv__(self, other):
+        if isinstance(other, Vector):
+            return Vector(*(self.array / other.array))
+        elif isinstance(other, int) or isinstance(other, float):
+            return Vector(*(self.array / other))
+        else:
+            self._check_vector_type(other)
 
-def v_scale(v, magnitude):
-    return [i * magnitude for i in v]
+    def __floordiv__(self, other):
+        if isinstance(other, Vector):
+            return Vector(*(self.array // other.array))
+        elif isinstance(other, int) or isinstance(other, float):
+            return Vector(*(self.array // other))
+        else:
+            self._check_vector_type(other)
 
+    def dot(self, other):
+        self._check_vector_type(other)
+        return np.dot(self.array, other.array)
 
-def v_len(v):
-    return math.sqrt(sum([i * i for i in v]))
+    def s_cross(self, other):
+        self._check_vector_type(other)
+        return self.array[0] * other.array[1] - self.array[1] * other.array[0]
 
+    def length(self):
+        return np.sqrt(np.sum(self.array ** 2))
 
-def v_par(v, magnitude):
-    length = v_len(v)
-    if length < 3:
-        return [magnitude, 0]
-    return [magnitude * i / length for i in v]
+    def unit_vector(self):
+        return Vector(*(self.array / self.length()))
 
+    def rotate_by_angle(self, angle_in_radians):
+        rotation_matrix = np.array([[np.cos(angle_in_radians), -np.sin(angle_in_radians)],
+                                    [np.sin(angle_in_radians), np.cos(angle_in_radians)]])
+        return Vector(*(rotation_matrix @ self.array))
 
-def v_turn(v1, v2):
-    magnitude = math.sqrt(v1[0] * v1[0] + v1[1] * v1[1]) * math.sqrt(v2[0] * v2[0] + v2[1] * v2[1])
-    turn_cos = (v1[0] * v2[0] + v1[1] * v2[1]) / magnitude
-    turn_sin = (v1[0] * v2[1] - v1[1] * v2[0]) / magnitude
-    return turn_cos, turn_sin
+    def rotate_by_turns(self, turns):
+        return self.rotate_by_angle(turns * 2.0 * np.pi)
 
+    def turn_angle(self, other):
+        self._check_vector_type(other)
+        magnitude = self.length() * other.length()
+        turn_cos = self.dot(other) / magnitude
+        turn_sin = self.s_cross(other) / magnitude
+        return turn_cos, turn_sin
 
-def v_rot(v, turns):
-    angle = 2.0 * math.pi * turns
-    return [v[0] * math.cos(angle) - v[1] * math.sin(angle), v[0] * math.sin(angle) + v[1] * math.cos(angle)]
+    def parallel(self, length):
+        this_length = self.length()
+        if this_length < .03:
+            return Vector(length, 0)
+        return Vector(*(self.array / this_length * length))
 
+    def distance(self, other):
+        self._check_vector_type(other)
+        return (self - other).length()
 
-def distance(loc1, loc2):
-    return math.sqrt((loc1[0] - loc2[0]) ** 2 + (loc1[1] - loc2[1]) ** 2)
+    def in_rect(self, rect):
+        return rect[0] <= self.array[0] <= rect[0] + rect[2] and \
+            rect[1] <= self.array[1] <= rect[1] + rect[3]
 
+    def as_tuple(self):
+        return float(self.array[0]), float(self.array[1])
 
-def fit_rect(image_rect, target_rect, expand=False):
-    if image_rect[2] <= target_rect[2] and image_rect[3] <= target_rect[3] and not expand:
-        scale = 1
-    else:
-        scale = min(target_rect[2] / image_rect[2], target_rect[3] / image_rect[3])
-    offset = (target_rect[0] + target_rect[2] // 2 - image_rect[0] - image_rect[2] // 2,
-              target_rect[1] + target_rect[3] // 2 - image_rect[1] - image_rect[3] // 2)
-    return offset, scale
+    def __getattr__(self, item):
+        if item == 'x':
+            return self.array[0]
+        elif item == 'y':
+            return self.array[1]
+        else:
+            return None
 
+    def __str__(self):
+        return f"Vector({self.array[0]}, {self.array[1]})"
 
-def text_box_size(text, font_size):
-    return [len(text) * 0.6 * font_size, font_size]
-
-
-def point_in_rect(point, rect):
-    return rect[0] <= point[0] <= rect[0] + rect[2] and \
-        rect[1] <= point[1] <= rect[1] + rect[3]
-
-
-def select_location(point,  list_of_locations, tolerance=7):
-    for i, location in enumerate(list_of_locations):
-        if distance(point, location) < tolerance:
-            return i
-    return None
+    def __repr__(self):
+        return str(self)
